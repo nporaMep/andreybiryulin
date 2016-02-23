b047443e90424e735cab569d0ac17a953094a66c
Andrey Biryulin <andrey.biryulin@hotmail.com>
2016-02-23 17:57:34 +0600

    Initial start page

--- src/andreybiryulin/Startup.cs
+++ src/andreybiryulin/Startup.cs
@@ -11,20 +11,14 @@ namespace andreybiryulin
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-
+        public void ConfigureServices(IServiceCollection services) => services
+            .AddMvc();
+        
+        public void Configure(IApplicationBuilder app) => app
+            .UseIISPlatformHandler()
+            .UseStaticFiles()
+            .UseMvcWithDefaultRoute();
+        
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
