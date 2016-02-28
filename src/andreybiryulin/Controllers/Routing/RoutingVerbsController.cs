2fd0b7781a8eabd8d3680648df26e0420063e5c2
Andrey Biryulin <andrey.biryulin@hotmail.com>
2016-02-28 12:51:09 +0600

    Routing verbs

---
+++ src/andreybiryulin/Controllers/Routing/RoutingVerbsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using AndreyBiryulin.ViewModels;
+
+namespace AndreyBiryulin.Controllers.Routing
+{
+    public class RoutingVerbsController : Controller
+    {
+        private RoutingInfo routingInfo;
+
+        public RoutingVerbsController(RoutingInfo routingInfo)
+        {
+            this.routingInfo = routingInfo;
+        }
+        [AcceptVerbs("GET")]
+        public IActionResult Entities() => View("RoutingInfo", routingInfo);
+        [HttpPost]
+        public IActionResult Entities(int id) => View("RoutingInfo", routingInfo);
+    }
+}
