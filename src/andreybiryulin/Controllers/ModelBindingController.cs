e7a4632590e6f9be7300a6b2c4e6bcbcf8e13e95
Andrey Biryulin <andrey.biryulin@hotmail.com>
2016-02-29 20:43:19 +0600

    Model binding basics

---
+++ src/andreybiryulin/Controllers/ModelBindingController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using AndreyBiryulin.ViewModels;
+using Microsoft.AspNet.Http;
+
+namespace AndreyBiryulin.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        private RoutingInfo routingInfo;
+
+        public ModelBindingController(RoutingInfo routingInfo)
+        {
+            this.routingInfo = routingInfo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FromQuery([FromQuery] int FromQuery) => View("RoutingInfo", routingInfo);
+
+        public IActionResult FromRoute([FromRoute(Name ="id")] int FromRoute) => View("RoutingInfo", routingInfo);
+
+        public string FromBody([FromBody] int fromBody)
+        {
+            return $"Message from server {fromBody}";
+        }
+
+        public IActionResult FromForm(IFormCollection formData, [FromForm] string text1, [FromForm] int number1, [FromForm] string text2)
+        {
+            ViewBag.FormData = formData;
+            ViewBag.text1 = text1;
+            ViewBag.number1 = number1;
+            ViewBag.text2 = text2;
+            return View();
+        }
+
+        public IActionResult FormFile(IFormFile formFile) => View(formFile)
+    }
+}
